316b8b53ae201cd81a346b875d506cf5eca6eb88
Fazil Sheik <fazilsheik@aospa.co>
2026-04-01 22:58:56 +0530

    Revert "display: Add soong config for DS Merge Node"
    This reverts commit 0cedff03fe0a442153844e30c9357cb5daea25ad.

--- config/display-product.mk
+++ config/display-product.mk
@@ -168,11 +168,10 @@ SOONG_CONFIG_qtidisplay := drmpp headless llvmsa \
                            default var1 var2 var3 llvmcov  \
                            composer_version smmu_proxy \
                            ubwcp_headers hwasan mapper_ext \
-                           neo disable_ds_node
+                           neo
 
 # Soong Values
 SOONG_CONFIG_qtidisplay_neo := false
-SOONG_CONFIG_qtidisplay_disable_ds_node := false
 SOONG_CONFIG_qtidisplay_drmpp := true
 SOONG_CONFIG_qtidisplay_headless := false
 SOONG_CONFIG_qtidisplay_llvmsa := false
