CodeLinaro-mirror/la_platform_hardware_qcom_display

display: fix qssi 16.1.1 tag update compiler issues
Resolved issues on format specifier mismatches in print statements

Change-Id: I147fefc6d78a31968ff8114b0ecabe181a2ff7a4

composer/DisplayConfigAIDL.cpp
@@ -428,7 +428,7 @@ ScopedAStatus DisplayConfigAIDL::getActiveBuiltinDisplayAttributes(Attributes *a
   error = settings_->GetDisplayAttributes(disp_id, config, &var_info);
 
   if (error != sdm::kErrorNone) {
-    ALOGW("%s: Invalid display = %llu", __FUNCTION__, disp_id);
+    ALOGW("%s: Invalid display = %lu", __FUNCTION__, disp_id);
     return ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
   }
 
@@ -735,7 +735,7 @@ ScopedAStatus DisplayConfigAIDL::setCWBOutputBuffer(
       cwb_callbacks_.insert({hdl, {display_type, callback}});
     }
   } else if (hdl_exists) {
-    ALOGE("%s: buffer(0x%x) already being handled by display-%d", __FUNCTION__, hdl, display_type);
+      ALOGE("%s: buffer(0x%p) already being handled by display-%d", __FUNCTION__, hdl, display_type);
   }
 
   if (ret_status != EX_NONE) {
@@ -764,11 +764,11 @@ void DisplayConfigAIDL::NotifyCWBStatus(int32_t status, void *hdl) {
   }
 
   if (!callback) {
-    ALOGE("%s: buffer handle(0x%x) not found", __FUNCTION__, hdl);
+    ALOGE("%s: buffer handle(0x%p) not found", __FUNCTION__, hdl);
   } else {
     NativeHandle buffer =
         sdm::AIDLNativeHandleFromSnapHandle(reinterpret_cast<SnapHandle *>(hdl), false);
-    ALOGI("%s: Notify the client about buffer (0x%x) status %d for display-%d.", __FUNCTION__, hdl,
+    ALOGI("%s: Notify the client about buffer (0x%p) status %d for display-%d.", __FUNCTION__, hdl,
           status, display_type);
 
     callback->notifyCWBBufferDone(status, buffer);

aospa-op13/android_hardware_qcom_display

composer/DisplayConfigAIDL.cpp
@@ -429,7 +429,7 @@ ScopedAStatus DisplayConfigAIDL::getActiveBuiltinDisplayAttributes(Attributes *a
   error = settings_->GetDisplayAttributes(disp_id, config, &var_info);
 
   if (error != sdm::kErrorNone) {
-    ALOGW("%s: Invalid display = %d", __FUNCTION__, disp_id);
+    ALOGW("%s: Invalid display = %ld", __FUNCTION__, disp_id);
     return ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
   }
 
@@ -740,7 +740,7 @@ ScopedAStatus DisplayConfigAIDL::setCWBOutputBuffer(
       cwb_callbacks_.insert({hdl, {display_type, callback}});
     }
   } else if (hdl_exists) {
-    ALOGE("%s: buffer(0x%x) already being handled by display-%d", __FUNCTION__, hdl, display_type);
+      ALOGE("%s: buffer(0x%p) already being handled by display-%d", __FUNCTION__, hdl, display_type);
   }
 
   if (ret_status != EX_NONE) {
@@ -769,11 +769,11 @@ void DisplayConfigAIDL::NotifyCWBStatus(int32_t status, void *hdl) {
   }
 
   if (!callback) {
-    ALOGE("%s: buffer handle(0x%x) not found", __FUNCTION__, hdl);
+    ALOGE("%s: buffer handle(0x%p) not found", __FUNCTION__, hdl);
   } else {
     NativeHandle buffer =
         sdm::AIDLNativeHandleFromSnapHandle(reinterpret_cast<SnapHandle *>(hdl), false);
-    ALOGI("%s: Notify the client about buffer (0x%x) status %d for display-%d.", __FUNCTION__, hdl,
+    ALOGI("%s: Notify the client about buffer (0x%p) status %d for display-%d.", __FUNCTION__, hdl,
           status, display_type);
 
     callback->notifyCWBBufferDone(status, buffer);